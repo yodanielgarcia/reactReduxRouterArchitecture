d62798ba11eb49e0a879c883b4c0ee78038a3792
Daniel Felipe Garcia Forero <dagarcia@belatrix.com>
2020-03-11 10:49:05 -0500

    structure react redux and router

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This app uses REact, Redux, React Router and a variety of ohter help</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
